dab389a6e5b4a83094561f11c92c9bde3660f58a
erroljh <84386522+erroljh@users.noreply.github.com>
2025-03-24 19:29:03 +0200

    Update CardService.kt
    the update includes Error handling for getCardByIndex() using NoSuchElementException.
    A function getCards(count: Int) to retrieve multiple cards.
    Card generation logic using the words from

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/card/CardService.kt
@@ -1,12 +1,39 @@
 package jetbrains.kotlin.course.alias.card
 
+import jetbrains.kotlin.course.alias.util.Words
 import org.springframework.stereotype.Service
+import java.util.NoSuchElementException
 
 @Service
 class CardService {
-    private fun generateCards(): List<Card> = TODO("Not implemented yet")
+    private var allCards: List<Card> = emptyList()
 
-    private fun List<String>.toWords(): List<Word> = TODO("Not implemented yet")
+    private fun generateCards(): List<Card> {
+        val words = Words.words.toWords()
+        val cards = words.map { Card(it) }
+        allCards = cards
+        return cards
+    }
 
-    fun getCardByIndex(index: Int): Card = TODO("Not implemented yet")
+    private fun List<String>.toWords(): List<Word> = this.map { Word(it) }
+
+    fun getCardByIndex(index: Int): Card {
+        if (allCards.isEmpty()) {
+            generateCards()
+        }
+        if (index < 0 || index >= allCards.size) {
+            throw NoSuchElementException("Card index is out of bounds: $index")
+        }
+        return allCards[index]
+    }
+
+    fun getCards(count: Int): List<Card> {
+        if (allCards.isEmpty()) {
+            generateCards()
+        }
+        if (count > allCards.size) {
+            return allCards
+        }
+        return allCards.subList(0, count)
+    }
 }
