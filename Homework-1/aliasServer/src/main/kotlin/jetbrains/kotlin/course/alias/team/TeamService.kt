74a696bf23674e7966fcf5c9fe7a1aa6e224241d
erroljh <84386522+erroljh@users.noreply.github.com>
2025-03-24 19:33:35 +0200

    Update TeamService.kt
    this does the following:
    
    Handles the case where teamsNumber is not positive.
    Creates a list of Team objects with names like "Team 1", "Team 2", etc.
    Returns an unmodifiable copy of the list of teams.

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
@@ -1,9 +1,19 @@
 package jetbrains.kotlin.course.alias.team
 
 import org.springframework.stereotype.Service
+import java.util.ArrayList
 
 @Service
 class TeamService {
 
-    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> = TODO("Not implemented yet")
+    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> {
+        if (teamsNumber <= 0) {
+            return emptyList()
+        }
+        val teams = ArrayList<Team>()
+        for (i in 1..teamsNumber) {
+            teams.add(Team("Team $i"))
+        }
+        return teams.toList()
+    }
 }
